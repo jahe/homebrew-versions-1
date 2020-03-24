75ac092d99ccef77f98f9751ccac4750b4ff28ce
Jannik Hell <trashkid0+git@gmail.com>
2020-03-24 17:42:32 +0100

    Update protobuf250.rb

--- protobuf250.rb
+++ protobuf250.rb
@@ -18,7 +18,7 @@ class Protobuf250 < Formula
   # this will double the build time approximately if enabled
   option "with-check", "Run build-time check"
 
-  depends_on :python => :optional
+  depends_on "python" => :optional
 
   fails_with :llvm do
     build 2334
